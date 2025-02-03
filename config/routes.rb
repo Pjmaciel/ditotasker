701ba9dd82d442f07ec005840f488b4ee1fdffa0
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-03 19:28:52 -0300

    fix(Routes): Added root route to solve error 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #  principal
+  root 'pages#construction'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  # Static pages
+  get '/construction', to: 'pages#construction'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Sidekiq Web UI (only accessible to Admin in the future)
   mount Sidekiq::Web => '/sidekiq'
 end
